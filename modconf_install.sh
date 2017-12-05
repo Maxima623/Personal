71afa955e3db4016790f0e1c6a68950d81605551
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2017-12-05 12:35:51 +0800

    Add custom config installer for modprobe

---
+++ modconf_install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+if [ -e /sys/module/drm ]; then
+	wget -P /etc/modprobe.d - https://raw.githubusercontent.com/Maxima623/Personal/master/Modprobe/99_drm.conf
+fi
+
+if [ -e /sys/module/i915 ]; then
+	wget -P /etc/modprobe.d - https://raw.githubusercontent.com/Maxima623/Personal/master/Modprobe/99_i915.conf
+fi
+
+if [ -e /sys/module/iwlwifi ]; then
+	wget -P /etc/modprobe.d - https://raw.githubusercontent.com/Maxima623/Personal/master/Modprobe/99_iwlwifi.conf
+fi
+
+exit 0
